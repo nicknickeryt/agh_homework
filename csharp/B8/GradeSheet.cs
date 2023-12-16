52258498d37e0ad0538dd5e851acc00bc04eb8c2
Nick Nicker <nick4nicker@gmail.com>
2023-12-16 23:20:50 +0100

    Add initial B8

---
+++ csharp/B8/GradeSheet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace B8
+{
+
+    public enum Grupa
+    {
+        GRUPA_1,
+        GRUPA_2,
+        GRUPA_3
+    }
+    class GradeSheet
+    {
+        private JsonSerializerOptions opts = new JsonSerializerOptions() { WriteIndented = true };
+        public string NameSurname { get; set; }
+        public Grupa Group { get; set; }
+
+        public Dictionary<string, float> GradeDictionary { get; set; }
+
+        public double AverageGrade()
+        {
+            double sum = 0;
+            int n = GradeDictionary.Count;
+            foreach (KeyValuePair<string, float> kvp in GradeDictionary)
+            {
+                sum += kvp.Value;
+            }
+            return sum / n;
+        }
+
+        public void Serialize(string filePath)
+        {
+            string gradesToJson = JsonSerializer.Serialize(this, opts);
+            try
+            {
+                File.WriteAllText(filePath, gradesToJson);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nie ma takiego pliku lub nie jest dostępny.");
+            }
+        }
+
+        public static GradeSheet Deserialize(string filePath)
+        {
+            try {
+                string jsonFromFile = File.ReadAllText(filePath);
+                GradeSheet gradesFromJson = JsonSerializer.Deserialize<GradeSheet>(jsonFromFile);
+                return gradesFromJson;
+            } catch (Exception) {
+                Console.WriteLine("Nie ma takiego pliku lub nie jest dostępny.");
+            }
+            return null;
+        }
+    }
+}
---
+++ csharp/B8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace B8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeSheet gradeSheet = new GradeSheet();
+
+            gradeSheet.NameSurname = "Jaś Kowalski";
+            gradeSheet.GradeDictionary = new Dictionary<string, float>{ {"analiza", 1} };
+            gradeSheet.Group = Grupa.GRUPA_3;
+            Console.WriteLine(gradeSheet.AverageGrade());
+
+            gradeSheet.Serialize("/home/nick/Dokumenty/AGH/infa/agh_homework/csharp/B8/testowyJson.json");
+
+            GradeSheet gradeSheet1 = GradeSheet.Deserialize("/home/nick/Dokumenty/AGH/infa/agh_homework/csharp/B8/testowyJson.json");
+
+
+            Console.WriteLine(gradeSheet1.AverageGrade());
+            Console.WriteLine(gradeSheet1.NameSurname);
+            Console.WriteLine(gradeSheet1.Group);
+        }
+    }
+}
